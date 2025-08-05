fbcfd3454d7df420d2300594dddbe963bf969f14
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-08-05 11:17:47 +0200

    initial commit

---
+++ src/main/java/com/github/egnbjork/roomallocator/RoomAllocatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.egnbjork.roomallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomAllocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomAllocatorApplication.class, args);
+    }
+
+}
